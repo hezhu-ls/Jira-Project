c6bba25cc78e4481c83a58472992495190d1f0c1
hezhuLightspeed <he.zhu@lightspeedhq.com>
2022-11-06 14:46:26 -0500

    first

---
+++ src/util/index.js
@@ -0,0 +1,27 @@
+import { useEffect } from "react";
+
+export const isFalsy = (value) => (value === 0 ? false : !value);
+
+// 在函数中，改变传入对象本身是不好的
+export const cleanObject = (object) => {
+  // Object.assign({}, object)
+  const result = { ...object };
+
+  Object.keys(result).forEach((key) => {
+    // exclude 0, which is true
+    const value = result[key];
+    if (isFalsy(value)) {
+      delete result[key];
+    }
+  });
+
+  return result;
+};
+
+// hook 里面使用 hook
+// 封装初始化 useEffect(), 去除结尾的 []
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback();
+  }, []);
+};
